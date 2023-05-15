6142710485c210b1bb0996d7963ac61e801b25d1
totto1998 <tottocarlos98@gmail.com>
2023-05-15 15:05:06 -0500

    S

--- resources/views/insumos/create.blade.php
+++ resources/views/insumos/create.blade.php
@@ -3,7 +3,11 @@
 @lang('translation.create-product')
 @endsection
 @section('css')
-<link href="{{ URL::asset('build/libs/dropzone/dropzone.css') }}" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/dropzone@5.9.3/dist/dropzone.css">
+
+{{--  <link href="{{ URL::asset('build/libs/dropzone/dropzone.css') }}" rel="stylesheet">  --}}
+
 <link rel="stylesheet" href="{{ URL::asset('build/css/style.css') }}">
 @endsection
 @section('content')
@@ -17,11 +21,11 @@ Crear insumos
 @endcomponent
 
 
-<form id="createproduct-form" autocomplete="off" class="needs-validation" novalidate>
+<form action="{{ route('insumos.store') }}" method="POST" enctype="multipart/form-data" id="createproduct-form" autocomplete="off" class="needs-validation" novalidate>
+    @csrf
     <div class="row">
         <div class="col-lg-8">
             <!-- end card -->
-
             <div class="card">
                 <div class="card-header">
                     <h5 class="card-title mb-0">Galeria de productos</h5>
@@ -32,7 +36,7 @@ Crear insumos
 
                         <div class="dropzone">
                             <div class="fallback">
-                                <input name="file" type="file" multiple="multiple">
+                                <input name="img" type="file" multiple="multiple">
                             </div>
                             <div class="dz-message needsclick">
                                 <div class="mb-3">
@@ -92,7 +96,7 @@ Crear insumos
                                 <div class="col-lg-3 col-sm-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="stocks-input">Stocks</label>
-                                        <input type="text" class="form-control" id="stocks-input" placeholder="Stocks" required pattern="[A-Za-z\s]+">
+                                        <input type="text" class="form-control" id="stocks-input" name="stock" placeholder="Stocks" required pattern="[A-Za-z\s]+">
                                         <div class="invalid-feedback">Ingrese cuantos productos estan en Stocks.</div>
                                     </div>
                                 </div>
@@ -101,7 +105,7 @@ Crear insumos
                                         <label class="form-label" for="product-price-input">Precio</label>
                                         <div class="input-group has-validation mb-3">
                                             <span class="input-group-text" id="product-price-addon">$</span>
-                                            <input type="text" class="form-control" id="product-price-input" placeholder="Enter price" aria-label="Price" aria-describedby="product-price-addon" required pattern="[0-9]+">
+                                            <input type="text" class="form-control" id="product-price-input" placeholder="Enter price" name="precio" aria-label="Price" aria-describedby="product-price-addon" required pattern="[0-9]+">
                                             <div class="invalid-feedback">Ingrese el precio.</div>
                                         </div>
 
@@ -112,7 +116,7 @@ Crear insumos
                                         <label class="form-label" for="product-discount-input">Descuento</label>
                                         <div class="input-group mb-3">
                                             <span class="input-group-text" id="product-discount-addon">%</span>
-                                            <input type="text" class="form-control" id="product-discount-input" placeholder="Descuento" aria-label="discount" aria-describedby="product-discount-addon" required pattern="[0-9]+">
+                                            <input type="text" class="form-control" id="product-discount-input" name="descuento" placeholder="Descuento" aria-label="discount" aria-describedby="product-discount-addon" required pattern="[0-9]+">
                                         </div>
                                     </div>
                                 </div>
@@ -136,11 +140,14 @@ Crear insumos
                     <label for="choices-publish-status-input" class="form-label">Categoria de insumos</label>
                     <select class="form-select" id="choices-publish-status-input" data-choices data-choices-search-false>
                         <option value="">Selecciona una opción</option>
-                        <option value="Tela">Tela</option>
-                        <option value="boton">boton</option>
-                        <option value="cremallera">cremallera</option>
+                        @foreach($paramcateg as $param)
+                            @if($param->id_tipo == 1)
+                                <option value="{{ $param->nombre }}">{{ $param->nombre }}</option>
+                            @endif
+                        @endforeach
                     </select>
                 </div>
+                
 
                 <div id="card-body-container"></div>
 
@@ -211,7 +218,7 @@ Crear insumos
                           </div>
                       </div>
               `;
-                        } else if (selectedValue === 'boton') {
+                        } else if (selectedValue === 'Botón' || selectedValue === 'Cremalleras' ) {
                             // Agregar el código HTML al contenedor
                             cardBodyContainerEl.innerHTML = `
               <div class="card-body">
@@ -275,54 +282,6 @@ Crear insumos
                           </div>
                       </div>
               `;
-                        } else if (selectedValue === 'cremallera') {
-                            // Agregar el código HTML al contenedor
-                            cardBodyContainerEl.innerHTML = `
-              <div class="card-body">
-                      <div class="mb-2">
-                          <label for="choices-publish-status-input" class="form-label">Unidad de medida</label>
-                              <select class="form-select" id="choices-publish-status-input" data-choices data-choices-search-false required>
-                                  <option value="">Metro</option>
-                                  <option value="Lino">Yarda</option>
-                                  <option value="Algodon">Bolsa</option>
-                                  <option value="boton">Kilogramo</option>
-                          </select>
-                      </div>
-                      <div class="mb-2">
-                          <label for="choices-publish-status-input" class="form-label">Color</label>
-                              <select class="form-select" id="choices-publish-status-input" data-choices data-choices-search-false required>
-                                  <option value="">Amarrillo</option>
-                                  <option value="Rojo">Rojo</option>
-                                  <option value="Azul">Azul</option>
-                          </select>
-                      </div>
-                      <div class="mb-3">
-                          <label for="ancho-input" class="form-label">Ancho del cremallera</label>
-                          <input type="text" id="ancho-input" class="form-control" required pattern="[A-Za-z]+" placeholder="Ingrese ancho de la tela">
-                      </div>
-                  </div>
-                  
-                  <div class="mb-2">
-                          <label for="choices-publish-status-input" class="form-label">Estado</label>
-                              <select class="form-select" id="choices-publish-status-input" data-choices data-choices-search-false required>
-                                  <option value="amarillo">Terminado</option>
-                                  <option value="rojo">Emproduccion</option>
-                          </select>
-                      </div>
-                  <div class="card">
-                          <div class="card-header">
-                              <h5 class="card-title mb-0">Tags de insumos</h5>
-                          </div>
-                          <div class="card-body">
-                              <div class="hstack gap-3 align-items-start">
-                                  <div class="flex-grow-1">
-                                      <input class="form-control" data-choices data-choices-multiple-remove="true" placeholder="Enter tags" type="text"
-                               value="" required pattern="[A-Za-z]+" />
-                               </div>
-                           </div>
-                          </div>pa
-                      </div>
-              `;
                         } else {
                             // Limpiar el contenido del contenedor si no se selecciona una opción correspondiente
                             cardBodyContainerEl.innerHTML = '';
@@ -331,16 +290,13 @@ Crear insumos
                 </script>
             </div>
         </div>
-    </div>
-    <div class="content">
-        <form action="#">
+        <div class="content">
             <div class="user-details">
                 <div class="button">
                     <input type="submit" value="Register">
                 </div>
             </div>
-        </form>
-    </div>
+        </div>
     </div>
     <!-- end row -->
 </form>
@@ -348,10 +304,36 @@ Crear insumos
 @endsection
 @section('script')
 <script src="{{ URL::asset('build/libs/@ckeditor/ckeditor5-build-classic/build/ckeditor.js') }}"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/dropzone@5.9.3/dist/dropzone.js"></script>
+
+<script>
+    $(document).ready(function () {
+        // Inicializar Dropzone
+        Dropzone.autoDiscover = false;
+        var maxFilesize = 2; // Tamaño máximo del archivo en MB
+        var myDropzone = new Dropzone('.dropzone', {
+            paramName: 'img', // Nombre del parámetro que enviará el archivo
+            maxFiles: 1, // Número máximo de archivos permitidos
+            maxFilesize: maxFilesize, // Tamaño máximo del archivo en MB
+            acceptedFiles: 'image/*', // Tipos de archivo aceptados (en este caso, solo imágenes)
+            dictDefaultMessage: 'Haz clic aquí para subir la imagen', // Mensaje predeterminado
+            dictFileTooBig: 'El archivo es demasiado grande. Tamaño máximo permitido: ' + maxFilesize + 'MB.', // Mensaje de error para archivos demasiado grandes
+            dictInvalidFileType: 'No se permite este tipo de archivo. Solo se admiten imágenes.', // Mensaje de error para tipos de archivo no permitidos
+            addRemoveLinks: true, // Mostrar enlaces para eliminar archivos cargados
+
+            // Otras opciones y eventos de Dropzone según sea necesario
+        });
+    });
+</script>
 
 
-<script src="{{ URL::asset('build/libs/dropzone/dropzone-min.js') }}"></script> 
+
+{{--  <script src="{{ URL::asset('build/libs/dropzone/dropzone-min.js') }}"></script>   --}}
 <script src="{{ URL::asset('build/js/pages/ecommerce-product-create.init.js') }}"></script>
 
 <script src="{{ URL::asset('build/js/app.js') }}"></script>
