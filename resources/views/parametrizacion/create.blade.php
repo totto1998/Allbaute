4f486f22c98182b6128410a4785aa782364950a2
Juan Alvarez <123114067+Sokiro02@users.noreply.github.com>
2023-05-29 14:17:09 -0500

    parametrizacion validado

--- resources/views/parametrizacion/create.blade.php
+++ resources/views/parametrizacion/create.blade.php
@@ -18,6 +18,11 @@ Crear nueva parametrizacion
 
 <form action="{{ route('parametrizacion.store') }}" method="POST" enctype="multipart/form-data" id="createproduct-form" autocomplete="off" class="needs-validation">
     @csrf
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
     <div class="row">
         <div class="col-lg-8">
             <div class="card">
@@ -25,7 +30,7 @@ Crear nueva parametrizacion
                     <div class="mb-3">
                         <label for="comentario" class="form-label">Comentario</label>
                         <textarea required class="form-control" id="comentario" name="comentario" rows="5"></textarea>
-                    </div>                    
+                    </div>
                 </div>
             </div>
         </div>
@@ -44,24 +49,26 @@ Crear nueva parametrizacion
 
                     <div id="campo_nombre" style="display:none;">
                         <label for="nombre_categoria" class="form-label">Nombre del parámetro</label>
-                        <input type="text" class="form-control" id="nombre_categoria" name="nombre_categoria" pattern="[a-zA-Z]+">
+                        <input type="text" class="form-control" id="nombre_categoria" name="nombre_categoria" pattern="[a-zA-Z\s]+" oninput="bloquearComillas('nombre_categoria')">
+                        <p id="mensajeError_categoria" class="error"></p>
                     </div>
 
                     <div id="campo_nombre2" style="display:none;">
                         <label for="nombre_sub_categoria" class="form-label">Nombre de la subcategoría</label>
-                        <input type="text" class="form-control" id="nombre_sub_categoria" name="nombre_sub_categoria" pattern="[a-zA-Z]+">
+                        <input type="text" class="form-control" id="nombre_sub_categoria" name="nombre_sub_categoria" pattern="[a-zA-Z\s]+" oninput="bloquearComillas('nombre_sub_categoria')">
+                        <p id="mensajeError" class="error"></p>
                     </div>
-                    
+
                     <div id="campo_categoria" style="display:none;">
                         <label for="categoria" class="form-label">Categorías</label>
                         <select class="form-select" id="categoria" name="categoria">
                             <option value="">Seleccionar categoría</option>
                             @foreach ($categorias as $categoria)
-                                <option value="{{ $categoria->id }}">{{ $categoria->nombre_categoria }}</option>
+                            <option value="{{ $categoria->id }}">{{ $categoria->nombre_categoria }}</option>
                             @endforeach
                         </select>
                     </div>
-                    
+
                     <div id="campo_estado" style="display:none;">
                         <label for="estado_categoria" class="form-label">Estado</label>
                         <select class="form-select" id="estado_categoria" name="estado_categoria">
@@ -77,18 +84,37 @@ Crear nueva parametrizacion
     <div class="content">
         <div class="user-details">
             <div class="button">
-                <input type="submit" name="submit" value="Enviar">
+                <input onclick="return validarCampoNombre()" type="submit" name="submit" value="Enviar">
             </div>
         </div>
     </div>
 </form>
 
-
-
-@endsection
-
-@section('script')
 <script>
+    function bloquearComillas(id) {
+        var input = document.getElementById(id);
+        input.value = input.value.replace(/['="]/g, '');
+    }
+
+    function validarCampoNombre() {
+        var texto = document.getElementById("nombre_categoria").value;
+        var patron = /^[a-zA-Z\s]+$/;
+
+        if (!patron.test(texto)) {
+            document.getElementById("mensajeError_categoria").textContent = "El texto contiene caracteres no permitidos";
+            return false;
+        }
+
+        var comentario = document.getElementById("comentario").value;
+
+        if (comentario.includes("'") || comentario.includes('"')) {
+            document.getElementById("mensajeError_comentario").textContent = "El comentario contiene comillas";
+            return false;
+        }
+
+        return true;
+    }
+
     function mostrarCampo() {
         var tipoParametrizacion = document.getElementById("parametro").value;
         var campoNombre = document.getElementById("campo_nombre");
@@ -100,7 +126,7 @@ Crear nueva parametrizacion
             campoNombre.style.display = "block";
             campoNombre2.style.display = "none";
             campoCategoria.style.display = "none";
-            campoEstado.style.display = "block";
+            campoEstado.style.display = "none";
         } else if (tipoParametrizacion === "2") {
             campoNombre.style.display = "none";
             campoNombre2.style.display = "block";
@@ -114,5 +140,9 @@ Crear nueva parametrizacion
         }
     }
 </script>
+
+@endsection
+
+@section('script')
 <script src="{{ asset('build/js/app.js') }}"></script>
 @endsection
--- storage/framework/views/be4b516e789131d72c4036c39d281504.php
+++ storage/framework/views/be4b516e789131d72c4036c39d281504.php
@@ -3,8 +3,8 @@
 <?php echo app('translator')->get('translation.create-product'); ?>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('css'); ?>
-<link href="<?php echo e(URL::asset('build/libs/dropzone/dropzone.css')); ?>" rel="stylesheet">
-<link rel="stylesheet" href="<?php echo e(URL::asset('build/css/style.css')); ?>">
+<link href="<?php echo e(asset('build/libs/dropzone/dropzone.css')); ?>" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo e(asset('build/css/style.css')); ?>">
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('content'); ?>
 <?php $__env->startComponent('components.breadcrumb'); ?>
@@ -16,71 +16,107 @@ Crear nueva parametrizacion
 <?php $__env->endSlot(); ?>
 <?php echo $__env->renderComponent(); ?>
 
-<form action="<?php echo e(route('parametrizacion.store')); ?>" method="POST">
+<form action="<?php echo e(route('parametrizacion.store')); ?>" method="POST" enctype="multipart/form-data" id="createproduct-form" autocomplete="off" class="needs-validation">
     <?php echo csrf_field(); ?>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
     <div class="row">
         <div class="col-lg-8">
             <div class="card">
                 <div class="card-body">
                     <div class="mb-3">
-                        <label for="descripcion" class="form-label">Descripción</label>
-                        <textarea required pattern="[a-zA-Z]+" class="form-control" id="descripcion" name="descripcion" rows="10"></textarea>
+                        <label for="comentario" class="form-label">Comentario</label>
+                        <textarea required class="form-control" id="comentario" name="comentario" rows="5"></textarea>
                     </div>
                 </div>
             </div>
-
         </div>
 
         <div class="col-lg-4">
-    <div class="card">
-        <div class="card-body">
-            <div class="mb-3">
-                <label for="tipo_parametrizacion" class="form-label">Tipo de Parámetro</label>
-                <select class="form-select" id="tipo_parametrizacion" name="tipo_parametrizacion" required onchange="mostrarCampo()">
-                    <option value="">Seleccionar tipo de parámetro</option>
-                    <option value="1">Nuevo parámetro</option>
-                    <option value="2">Nueva categoria</option>
-                </select>
-            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="parametro" class="form-label">Tipo de Parámetro</label>
+                        <select class="form-select" id="parametro" name="parametro" required onchange="mostrarCampo()">
+                            <option value="">Seleccionar tipo de parámetro</option>
+                            <option value="1">Nuevo parámetro</option>
+                            <option value="2">Nueva categoría</option>
+                        </select>
+                    </div>
 
-            <div id="campo_nombre" style="display:none;">
-                <label for="nombre" class="form-label">Nombre del parámetro</label>
-                <input type="text" class="form-control" id="nombre" name="nombre" required pattern="[a-zA-Z]+">
-            </div>
+                    <div id="campo_nombre" style="display:none;">
+                        <label for="nombre_categoria" class="form-label">Nombre del parámetro</label>
+                        <input type="text" class="form-control" id="nombre_categoria" name="nombre_categoria" pattern="[a-zA-Z\s]+" oninput="bloquearComillas('nombre_categoria')">
+                        <p id="mensajeError_categoria" class="error"></p>
+                    </div>
 
-            <div id="campo_nombre2" style="display:none;">
-                <label for="nombre_categoria" class="form-label">Nombre de la subcategoria</label>
-                <input type="text" class="form-control" id="nombre_categoria" name="nombre_categoria" required pattern="[a-zA-Z]+">
-            </div>
+                    <div id="campo_nombre2" style="display:none;">
+                        <label for="nombre_sub_categoria" class="form-label">Nombre de la subcategoría</label>
+                        <input type="text" class="form-control" id="nombre_sub_categoria" name="nombre_sub_categoria" pattern="[a-zA-Z\s]+" oninput="bloquearComillas('nombre_sub_categoria')">
+                        <p id="mensajeError" class="error"></p>
+                    </div>
 
-            <div id="campo_categoria" style="display:none;">
-                <label for="categoria" class="form-label">Categorias</label>
-                <select class="form-select" id="categoria" name="categoria">
-                    <option value="">Seleccionar categoria</option>
-                    <option value="opcion1">Tela</option>
-                    <option value="opcion2">Botón</option>
-                    <option value="opcion3">Cremallera</option>
-                    <option value="opcion4">Metodo de pago</option>
-                    <option value="opcion5">Unidad de medida</option>
-                    <!-- Agrega más opciones según sea necesario -->
-                </select>
-            </div>
+                    <div id="campo_categoria" style="display:none;">
+                        <label for="categoria" class="form-label">Categorías</label>
+                        <select class="form-select" id="categoria" name="categoria">
+                            <option value="">Seleccionar categoría</option>
+                            <?php $__currentLoopData = $categorias; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $categoria): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <option value="<?php echo e($categoria->id); ?>"><?php echo e($categoria->nombre_categoria); ?></option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </select>
+                    </div>
 
-            <div id="campo_estado" style="display:none;">
-                <label for="estado" class="form-label">Estado</label>
-                <select class="form-select" id="estado" name="estado">
-                    <option value="">Seleccionar estado</option>
-                    <option value="1">Activo</option>
-                    <option value="0">Inactivo</option>
-                </select>
+                    <div id="campo_estado" style="display:none;">
+                        <label for="estado_categoria" class="form-label">Estado</label>
+                        <select class="form-select" id="estado_categoria" name="estado_categoria">
+                            <option value="">Seleccionar estado</option>
+                            <option value="1">Activo</option>
+                            <option value="0">Inactivo</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="content">
+        <div class="user-details">
+            <div class="button">
+                <input onclick="return validarCampoNombre()" type="submit" name="submit" value="Enviar">
+            </div>
+        </div>
+    </div>
+</form>
 
 <script>
+    function bloquearComillas(id) {
+        var input = document.getElementById(id);
+        input.value = input.value.replace(/['="]/g, '');
+    }
+
+    function validarCampoNombre() {
+        var texto = document.getElementById("nombre_categoria").value;
+        var patron = /^[a-zA-Z\s]+$/;
+
+        if (!patron.test(texto)) {
+            document.getElementById("mensajeError_categoria").textContent = "El texto contiene caracteres no permitidos";
+            return false;
+        }
+
+        var comentario = document.getElementById("comentario").value;
+
+        if (comentario.includes("'") || comentario.includes('"')) {
+            document.getElementById("mensajeError_comentario").textContent = "El comentario contiene comillas";
+            return false;
+        }
+
+        return true;
+    }
+
     function mostrarCampo() {
-        var tipoParametrizacion = document.getElementById("tipo_parametrizacion").value;
+        var tipoParametrizacion = document.getElementById("parametro").value;
         var campoNombre = document.getElementById("campo_nombre");
         var campoNombre2 = document.getElementById("campo_nombre2");
         var campoCategoria = document.getElementById("campo_categoria");
@@ -90,7 +126,7 @@ Crear nueva parametrizacion
             campoNombre.style.display = "block";
             campoNombre2.style.display = "none";
             campoCategoria.style.display = "none";
-            campoEstado.style.display = "block";
+            campoEstado.style.display = "none";
         } else if (tipoParametrizacion === "2") {
             campoNombre.style.display = "none";
             campoNombre2.style.display = "block";
@@ -105,26 +141,10 @@ Crear nueva parametrizacion
     }
 </script>
 
-
-    </div>
-    <div class="content">
-        <form action="#">
-            <div class="user-details">
-                <div class="button">
-                    <input type="submit" value="Register">
-                </div>
-            </div>
-        </form>
-    </div>
-</form>
-
-
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('script'); ?>
-<script src="<?php echo e(URL::asset('build/libs/@ckeditor/ckeditor5-build-classic/build/ckeditor.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/libs/dropzone/dropzone-min.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/pages/ecommerce-product-create.init.js')); ?>"></script>
-<script src="<?php echo e(URL::asset('build/js/app.js')); ?>"></script>
+<script src="<?php echo e(asset('build/js/app.js')); ?>"></script>
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\Allbaute\Allbaute\resources\views/parametrizacion/create.blade.php ENDPATH**/ ?>
